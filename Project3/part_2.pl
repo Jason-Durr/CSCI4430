8314f91027595b4b1ef738d68af7c0e96a4d5851
Jason-Durr <durrj2@rpi.edu>
2019-12-02 17:17:06 -0500

    works but doesnt print

--- Project3/part_2.pl
+++ Project3/part_2.pl
@@ -14,6 +14,53 @@ integers --> ["integers"].
 that --> ["that"].
 to --> ["to"].
 
+% Function that takes in two numbers and does the correct operation and puts it in res
+
+% Base Case if there are no mpore even or odd to use the rsult is if Start is Goal
+process(Start,Goal, 0,0,_,_) :- Start is Goal.
+%otherwise find the next even number and add it
+process(Start,Goal, NumEven,NumOdd,"sum",Last) :- 
+    NumEven > 0,
+    between(Last,120,N1),
+    0 is N1 mod 2,
+    NewEven is NumEven-1,
+    NewStart is Start + N1,
+    NewLast is N1 + 1,
+    process(NewStart,Goal,NewEven,NumOdd,"sum",NewLast),
+    write(N1),nl.
+% Otherwise find the next odd number and add it
+process(Start,Goal, 0 ,NumOdd,"sum",Last):- 
+    NumOdd > 0,
+    between(Last,120,N1),
+    1 is N1 mod 2,
+    NewOdd is NumOdd-1,
+    NewStart is Start + N1,
+    NewLast is N1 + 1,
+    process(NewStart,Goal,0,NewOdd,"sum",NewLast),
+    write(N1),
+    nl.
+% otherwisee multiply the next even number
+process(Start,Goal, NumEven,NumOdd,"multiply",Last) :- 
+    NumEven > 0,
+    between(Last,120,N1),
+    0 is N1 mod 2,
+    NewEven is NumEven-1,
+    NewStart is Start * N1,
+    NewLast is N1 + 1,
+    process(NewStart,Goal,NewEven,NumOdd,"multiply",NewLast),
+    write(N1),
+    nl.
+% otherwise multiply start by the next odd integer
+process(Start,Goal, 0 ,NumOdd,"multiply",Last):- 
+    NumOdd > 0,
+    between(Last,120,N1),
+    1 is N1 mod 2,
+    NewOdd is NumOdd-1,
+    NewStart is Start * N1,
+    NewLast is N1 + 1,
+    process(NewStart,Goal,0,NewOdd,"multiply",NewLast),
+    write(N1),
+    nl.
 
 %      Test cases to copy paste into terminal
 %      swipl -q -f part_2.pl -t main "Example Input String"
@@ -30,10 +77,19 @@ main(W) :-
     split_string(S, " ", "", L),
     (   
         % Case where they only ask for even OR odd numbers
-        input(Num1, Type, Oper, Num2, L, []) -> write(Num1), write(Type), write(Oper), write(Num2);
+        input(Num1, "even", "sum", Num2, L, []) -> process(0,Num2, Num1, 0,"sum",0);
 
+        input(Num1, "odd", "sum", Num2, L, []) ->  process(0,Num2, 0,Num1,"sum",0);
         % Case for when they ask for both even AND odd numbers
-        input(Num1, Type1, Num2, Type2, Oper, Num3, L, []) -> write(Num1), write(Type1), write(Num2), write(Type2), write(Oper), write(Num3);
+        input(Num1, _, Num2, _, "sum", Num3, L, []) -> process(0,Num3,Num1,Num2,"sum",0);
+
+        input(Num1, "even", "multiply", Num2, L, []) -> process(1,Num2, Num1, 0,"multiply",0);
+
+        input(Num1, "odd", "multiply", Num2, L, []) ->  process(1,Num2, 0,Num1,"multiply",0);
+        % Case for when they ask for both even AND odd numbers
+        input(Num1, _, Num2, _, "multiply", Num3, L, []) -> process(1,Num3,Num1,Num2,"multiply",0);
+
+    
         
         % Otherwise it is an invalid string
         write("Invalid String")
